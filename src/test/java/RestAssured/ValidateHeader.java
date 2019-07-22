7cb23730a3a0cd56a53528f3aae968571999885e
unknown <sachin.s@tavant.com>
2019-07-23 00:40:07 +0530

    adding all header

--- src/test/java/RestAssured/ValidateHeader.java
+++ src/test/java/RestAssured/ValidateHeader.java
@@ -3,7 +3,10 @@ package RestAssured;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.http.Method;
+import io.restassured.internal.http.HttpResponseDecorator.HeadersDecorator;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
@@ -16,11 +19,20 @@ public class ValidateHeader {
 		
 		Response response=request.request(Method.GET,"?pages=9");
 		
-		String cache=response.header("cache-control");
+		/*String cache=response.header("cache-control");
 		System.out.println(cache);
 		
 		String content=response.header("etag");
-		System.out.println(content);
+		System.out.println(content);*/
+		
+		//get All hearder from API
+		Headers allheaders=response.headers();
+		
+		for(Header header:allheaders) {
+			System.out.println(header.getName()+"  "+header.getValue());
+			
+		}
+		
 		
 	}
 
